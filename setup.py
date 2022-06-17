d6904e8d5591fef0602006e80926a05ac78a042c
Valentin Goldité <valentin.goldite@gmail.com>
2022-06-18 00:57:05 +0200

    ➕ Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Setup of gan-face-editing."""
+
+from setuptools import find_packages, setup
+
+# Installation
+config = {
+    'name': 'gan-face-editing-2',
+    'version': '1.0.0',
+    'description': 'Inter Centrales Ceteris Paribus face challenge, 2nd team.',
+    'author': 'Hédi Razgallah',
+    'packages': find_packages(),
+    'zip_safe': True
+}
+
+setup(**config)
